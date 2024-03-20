e13871373bfdaf2b631b38ef7b4b2ff3d1d96668
navegantes <raphaelnavegantes@gmail.com>
2024-03-20 11:30:50 -0300

    add singleton with enum protection

--- Java/src/com/nave/designpattern/Singleton/solucao/TesteAgendaComSingleton.java
+++ Java/src/com/nave/designpattern/Singleton/solucao/TesteAgendaComSingleton.java
@@ -7,35 +7,54 @@ import java.lang.reflect.InvocationTargetException;
 public class TesteAgendaComSingleton
 {
   public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-    System.out.println("\nAgenda Singleton EAGER");
-    AgendaSingletonEAGER agenda1 = AgendaSingletonEAGER.getInstance();
-    AgendaSingletonEAGER agenda2 = AgendaSingletonEAGER.getInstance();
+//    System.out.println("\nAgenda Singleton EAGER");
+//    AgendaSingletonEAGER agenda1 = AgendaSingletonEAGER.getInstance();
+//    AgendaSingletonEAGER agenda2 = AgendaSingletonEAGER.getInstance();
+//
+//    System.out.println(agenda1);
+//    System.out.println(agenda2);
+//
+//    reservaDiaEAGER("Sexta");
+//    reservaDiaEAGER("Sabado");
+//
+//    System.out.println("\nAgenda Singleton LAZY");
+//
+//    AgendaSingletonLAZY agendalazy1 = AgendaSingletonLAZY.getInstance();
+//    AgendaSingletonLAZY agendalazy2 = AgendaSingletonLAZY.getInstance();
+//
+//    System.out.println(agendalazy1);
+//    System.out.println(agendalazy2);
+//
+//    // Reflection
+//    Constructor<AgendaSingletonLAZY> constructorCracked = AgendaSingletonLAZY.class.getDeclaredConstructor();
+//    constructorCracked.setAccessible(true);
+//    AgendaSingletonLAZY agendaCracked1 = constructorCracked.newInstance();
+//    AgendaSingletonLAZY agendaCracked2 = constructorCracked.newInstance();
+//
+//    System.out.println(agendaCracked1);
+//    System.out.println(agendaCracked2);
+//
+//    reservaDiaLAZY("Sexta");
+//    reservaDiaLAZY("Sabado");
 
-    System.out.println(agenda1);
-    System.out.println(agenda2);
+    System.out.println("\nAgenda Singleton Enum");
+    AgendaSingletonEnum agenda1 = AgendaSingletonEnum.getInstance();
+    AgendaSingletonEnum agenda2 = AgendaSingletonEnum.getInstance();
 
-    reservaDiaEAGER("Sexta");
-    reservaDiaEAGER("Sabado");
-
-    System.out.println("\nAgenda Singleton LAZY");
-
-    AgendaSingletonLAZY agendalazy1 = AgendaSingletonLAZY.getInstance();
-    AgendaSingletonLAZY agendalazy2 = AgendaSingletonLAZY.getInstance();
-
-    System.out.println(agendalazy1);
-    System.out.println(agendalazy2);
+    System.out.println(agenda1.hashCode());
+    System.out.println(agenda2.hashCode());
 
     // Reflection
-    Constructor<AgendaSingletonLAZY> constructorCracked = AgendaSingletonLAZY.class.getDeclaredConstructor();
+    Constructor<AgendaSingletonEnum> constructorCracked = AgendaSingletonEnum.class.getDeclaredConstructor();
     constructorCracked.setAccessible(true);
-    AgendaSingletonLAZY agendaCracked1 = constructorCracked.newInstance();
-    AgendaSingletonLAZY agendaCracked2 = constructorCracked.newInstance();
+    AgendaSingletonEnum agendaCracked1 = constructorCracked.newInstance();
+    AgendaSingletonEnum agendaCracked2 = constructorCracked.newInstance();
 
     System.out.println(agendaCracked1);
     System.out.println(agendaCracked2);
 
-    reservaDiaLAZY("Sexta");
-    reservaDiaLAZY("Sabado");
+    reservaDiaEnum("Sexta");
+    reservaDiaEnum("Sabado");
   }
 
   public static void reservaDiaEAGER(String data)
@@ -49,6 +68,14 @@ public class TesteAgendaComSingleton
   public static void reservaDiaLAZY(String data)
   {
     AgendaSingletonLAZY agenda = AgendaSingletonLAZY.getInstance();
+    agenda.ocupa(data);
+
+    System.out.println(agenda.getDias());
+  }
+
+  public static void reservaDiaEnum(String data)
+  {
+    AgendaSingletonEnum agenda = AgendaSingletonEnum.getInstance();
     agenda.ocupa(data);
 
     System.out.println(agenda.getDias());
